3a908a6a564ae37dd84a19ea5ab600f5b8961fa6
Jeff Ma <b9403206@gmail.com>
2024-11-03 00:20:13 +0800

    另存json試試看

--- app.py
+++ app.py
@@ -48,8 +48,10 @@ static_tmp_path = '.'
 #url = 'https://notify-api.line.me/api/notify'
 creds_json = json.loads(os.getenv("GOOGLE_SHEETS_CREDS"))
 print(creds_json)
+with open('creds.json', 'w') as json_file:
+    json.dump(creds_json, json_file, indent=4)  # indent 用於美化格式
 # 使用 from_client_config 進行授權
-gc = pygsheets.authorize(client_config=creds_json)
+gc = pygsheets.authorize(service_account_file=creds.json)
 survey_url = 'https://docs.google.com/spreadsheets/d/1LffAHLYbv6bOgovVwmUZcBO2WzAy0WmxbNQx8wFHbhk/edit?usp=sharing'
 sh = gc.open_by_url(survey_url)
 
