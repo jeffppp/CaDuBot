ce537363229949c49570da71271eafa429e78cb9
Jeff Ma <b9403206@gmail.com>
2024-11-03 15:55:04 +0800

    修迴圈bug

--- send_push_message.py
+++ send_push_message.py
@@ -44,9 +44,9 @@ def getResponse(content, line_bot_api, sh):
             column_a_values = ws.get_col(1)  # A 欄為 1，傳回值為列表
             column_c_values = ws.get_col(3)  # C 欄為 3，傳回值為列表
             # 輸出結果
-            for value,strdata in column_a_values,column_c_values:
-                message = TextSendMessage(text=strdata)
-                line_bot_api.push_message(value, message)        
+            for value,strdata in zip(column_a_values, column_c_values):
+                message = TextSendMessage(text=strdata[0])
+                line_bot_api.push_message(value[0], message)        
         return []
     except LineBotApiError as e:
         error = '''LineBotApiError\n''' + e.__str__()
