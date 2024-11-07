abb63f33d5b5fafa063b74b6300d76ee6aac4724
Jeff Ma <b9403206@gmail.com>
2024-11-07 11:11:43 +0800

    使用者名稱更為所有人

--- app.py
+++ app.py
@@ -122,11 +122,17 @@ def handle_message(event):
         ws = sh.worksheet_by_title('聊天室資料')
         ws.cell((1,10)).set_value('=MATCH("'+room_id+'",A:A,0)')
         ws.refresh()
-        if(ws.cell((1,10)).value=='#N/A'):
+        if(ws.cell((1,10)).value!='#N/A'):
+            member_ids = line_bot_api.get_room_member_ids(room_id)
+            member_names = []
+            # 使用 get_profile 來獲取每位成員的名稱
+            for user_id in member_ids:
+                profile = line_bot_api.get_profile(user_id)
+                member_names.append(profile.display_name)
             ws.add_rows(1)
             L=len(ws.get_col(1,include_tailing_empty=False))
             ws.cell((L+1,1)).set_value(room_id)
-            ws.cell((L+1,2)).set_value(profile.display_name)
+            ws.cell((L+1,2)).set_value(member_names)
             message = TextSendMessage(text="已記錄視窗ID")
             line_bot_api.push_message(room_id, message)
         replyMessageList = []
